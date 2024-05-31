3a6ca83edd7497d604bea122bc46769aebeabf1b
moooooora <omarmohammed271@gmail.com>
2024-05-31 16:44:19 +0300

    added register

--- project/views.py
+++ project/views.py
@@ -5,21 +5,21 @@ from django.contrib.auth import authenticate,login,logout
 
 
 
-# @login_required(login_url='login')
+@login_required(login_url='login')
 def home(request):
     return render(request,'home.html')
 def register(request):
     if request.method =='POST':
-        username = request.POST['username']
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
-        if password1 == password2:
-            user_done = User()
-            user_done.username = username
-            user_done.set_password(password1)
-            user_done.save()
-            user = authenticate(username=username,password=password1)
-            login(request,user)
+        username = request.POST.get('username')
+        password1 = request.POST.get('password1')
+        
+        
+        user_done = User()
+        user_done.username = username
+        user_done.set_password(password1)
+        user_done.save()
+        user = authenticate(username=username,password=password1)
+        login(request,user)
 
         return redirect('home')
 
@@ -48,7 +48,7 @@ def logout_view(request):
 def new_meeting(request):
     
     return render(request,'videocall/streaming.html',{'username':request.user.username,})
-# @login_required(login_url='login')
+@login_required(login_url='login')
 def join_meeting(request):
     if request.method == "POST":
         roomID = request.POST['roomID']
